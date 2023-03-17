aa07356b98ee1d0094494818bd6da20220c10e95
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-03-17 14:28:29 +0700

    adding user_agents

--- spec/commands/api/v1/google/fetch_results_spec.rb
+++ spec/commands/api/v1/google/fetch_results_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Api::V1::Google::FetchResults, type: :model do
       { keywords: keywords }
     end
 
-    let(:max_keywords) { 10 }
+    let(:max_keywords) { 50 }
 
     let(:keywords) do
       (1..max_keywords).to_a.map do |i|
@@ -14,17 +14,16 @@ RSpec.describe Api::V1::Google::FetchResults, type: :model do
       end
     end
 
-    let(:google_search_response) do
-      VCR.use_cassette("google/searches_100") { GoogleScrape::Search.fetch_results(keywords) }
-    end
+    # let(:google_search_response) do
+    #   VCR.use_cassette("google/searches_100") { GoogleScrape::Search.fetch_results(keywords) }
+    # end
 
-    before do
-      allow_any_instance_of(described_class).to receive(:ait).and_return(google_search_response)
-    end
+    # before do
+    #   allow_any_instance_of(described_class).to receive(:ait).and_return(google_search_response)
+    # end
 
     it 'test' do
       cmd = described_class.call(params)
-      byebug
       p cmd.result
 
       expect(cmd.success?).to be(true)
