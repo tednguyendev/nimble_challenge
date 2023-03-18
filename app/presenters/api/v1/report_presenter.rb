7636173e20f1d256a2cea8b9b6bc45e481c1fc15
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-03-18 23:22:45 +0700

    Add status and percentage fields to report list

--- app/presenters/api/v1/report_presenter.rb
+++ app/presenters/api/v1/report_presenter.rb
@@ -5,6 +5,8 @@ module Api
         {
           id: entity.id,
           name: entity.name,
+          status: entity.status,
+          percentage: entity.percentage,
           created_at: entity.created_at.strftime('%Y-%m-%d %H:%M:%S'),
         }
       end
