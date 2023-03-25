445bbe28ce0dc1a27951c2ef72d7fc691faa3510
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-03-26 00:11:26 +0700

    Update get_user_agents_spec.rb

--- spec/commands/api/v1/reports/get_user_agents_spec.rb
+++ spec/commands/api/v1/reports/get_user_agents_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::Reports::GetUserAgents do
-  subject(:command) { described_class.new() }
-
-  it 'returns correct agents' do
-    expect(command.call.result.length).to eq(100)
+  it 'returns a random user agent string' do
+    user_agent = described_class.instance.random_user_agent
+    expect(described_class.instance.user_agents).to include(user_agent)
   end
 end
