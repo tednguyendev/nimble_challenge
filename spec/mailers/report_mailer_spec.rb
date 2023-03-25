cc27682756d1507f08fc4e01619f447eaa821403
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-03-25 19:32:00 +0700

    Update mailers

--- spec/mailers/report_mailer_spec.rb
+++ spec/mailers/report_mailer_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ReportMailer, type: :mailer do
-  describe '#report_status_success' do
+  describe '#report_status' do
     let(:report) { create(:report) }
     let(:report_id) { report.id }
-    let(:mail) { described_class.report_status_success(report_id).deliver_now }
+    let(:mail) { described_class.report_status(report_id).deliver_now }
 
     context 'report is not found' do
       let(:report_id) { 123 }
@@ -20,32 +20,7 @@ RSpec.describe ReportMailer, type: :mailer do
       end
 
       it 'send correctly' do
-        expect(mail.subject).to eq('Your report scraping is done!')
-        expect(mail.to).to eq([report.user.email])
-      end
-    end
-  end
-
-  describe '#report_status_fail' do
-    let(:report) { create(:report) }
-    let(:report_id) { report.id }
-    let(:mail) { described_class.report_status_fail(report_id).deliver_now }
-
-    context 'report is not found' do
-      let(:report_id) { 123 }
-
-      it 'does not send an email' do
-        expect { mail }.not_to change(ActionMailer::Base.deliveries, :count)
-      end
-    end
-
-    context 'report found' do
-      it 'send an email' do
-        expect { mail }.to change(ActionMailer::Base.deliveries, :count)
-      end
-
-      it 'send correctly' do
-        expect(mail.subject).to eq('Your report scraping is failed.')
+        expect(mail.subject).to eq('Your report scraping is processed!')
         expect(mail.to).to eq([report.user.email])
       end
     end
