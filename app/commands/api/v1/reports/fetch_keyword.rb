40ef0150d14e557b97c7f52c6f072664ad7a5301
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-03-26 12:03:45 +0700

    Separate the logic to parse HTML from FetchKeyword + Add tests

--- app/commands/api/v1/reports/fetch_keyword.rb
+++ app/commands/api/v1/reports/fetch_keyword.rb
@@ -11,10 +11,9 @@ module Api
 
         def call
           return unless keyword
-          print("========>keyword.value : ", keyword.value)
 
           begin
-            fetch
+            fetch_and_update_keyword
           rescue Exception => e
             print("========>e : ", e)
             mark_keyword_as_failed
@@ -25,27 +24,26 @@ module Api
 
         private
 
-        def fetch
+        def fetch_and_update_keyword
           html_string = get_html_string
-          # html_string = File.open(Rails.root.join('spec', 'mocks', 'crawl_api_google.html'), 'r').read
-          doc = Nokogiri::HTML(html_string)
+          result = parse_html_string(html_string)
 
-          if doc.css('#captcha-form').length >= 1
+          if result[:have_captcha]
             raise 'captcha'
           else
-            total_results, search_time = get_total_results_and_search_time(doc)
-            ad_words_count = get_ad_words_count(doc)
-            links_count = get_links_count(doc)
-
             keyword.update(
-              ad_words_count: ad_words_count,
-              links_count: links_count,
-              total_results: total_results,
-              search_time: search_time,
+              ad_words_count: result[:data][:ad_words_count],
+              links_count: result[:data][:links_count],
+              total_results: result[:data][:total_results],
+              search_time: result[:data][:search_time],
               html_string: html_string,
               status: Keyword::SUCCESS
             )
           end
+        end
+
+        def parse_html_string(html_string)
+          ParseHtmlString.call(html_string).result
         end
 
         def get_html_string
@@ -65,38 +63,6 @@ module Api
 
         def report
           @report ||= keyword.report
-        end
-
-        def get_total_results_and_search_time(doc)
-          total_results = nil
-          search_time = nil
-
-          result_stats_text = doc.css('#result-stats').text.strip
-
-          if result_stats_text.length.positive?
-            result_stats_ary = result_stats_text.split(" ")
-            total_results = result_stats_ary[1].gsub(".",",").gsub(",","").to_i
-            search_time = result_stats_ary[-2].gsub("(","").gsub(",",".").to_f
-          end
-
-          [total_results, search_time]
-        end
-
-        def get_ad_words_count(doc)
-          normal_ads_count = doc.css('.U3A9Ac.qV8iec').count
-
-          # sometimes there are 2 of them, but only show 1 in the page
-          shopping_ads_count = doc.css('.fryEeb.U3A9Ac.irmCpc').count > 0 ? 1 : 0
-          normal_ads_count + shopping_ads_count
-        end
-
-        def get_links_count(doc)
-          doc.css('a')
-             .map { |l| l['href'] }
-             .select { |l| l.is_a?(String)}
-             .uniq
-             .count
-             #  .select { |l| l.is_a?(String) && "http".in?(l) && !".google.com".in?(l) }
         end
 
         def update_report
