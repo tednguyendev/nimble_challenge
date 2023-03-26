4cd180c48f78f9e2ceaaecea2709badcb277d7a7
tednguyendev <76901736+tednguyendev@users.noreply.github.com>
2023-03-26 17:57:15 +0700

    twist some configs and try to deploy

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV['EMAIL']
+  default from: (ENV['EMAIL'] || 'from@example.com')
   layout 'mailer'
 end
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,6 @@
 set :stage, :staging
-set :branch, 'main'
+set :branch, 'set-up-ci-cd'
+# set :branch, 'main'
 set :rbenv_ruby, '2.6.6'
 
 set :rails_env, :staging
@@ -8,4 +9,4 @@ set :environment, 'staging'
 # config file
 set :enable_ssl, false
 
-server '18.136.207.28', user: 'ubuntu', roles: %w{app db web}
+server '18.136.207.28', user: 'ubuntu', roles: %w{app db web runner}
